5a0dd67c6fd3504d131d021082b994bf0ae21726
fil-felipe <filip.adamek@outlook.com>
2024-12-09 21:11:09 +0100

    add shopping list backend api

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 app.get('/api', (req, res) => {
     res.send({message: 'Hello from the backend!' });
@@ -20,16 +22,18 @@ app.get('/read-shopping-lists', async (req, res) => {
 
     try {
         const result = await pool.query('SELECT * FROM shopping_list');
+        // console.log(result);
         res.status(200).json(result.rows);
       } catch (err) {
         console.error('Błąd zapytania:', err);
         res.status(500).send('Błąd serwera');
       }
 
-    res.json({ message: result });
+    // res.json({ message: result.rows });
 })
 
 app.post('/add-list', async (req, res) => {
+    console.log(req.body);
     const { listName } = req.body;
 
     if (!listName) {
@@ -42,7 +46,7 @@ app.post('/add-list', async (req, res) => {
         const result = await pool.query('INSERT INTO shopping_list (name) VALUES ($1) RETURNING *', [listName]);
         res.status(200).json(result.rows);
       } catch (err) {
-        console.error('Błąd zapytania:', err);
+        console.error('Błąd zapytania:', err.stack);
         res.status(500).send('Błąd serwera');
       }
 
